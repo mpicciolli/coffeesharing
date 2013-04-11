a804f5c2c10f64b49ab10a0320ae83695c0ab0a0
William Hubault <william.hubault@gmail.com>
2013-04-11 02:01:37 +0200

    Make the font vary in the shop name header

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,12 @@
 module PlacesHelper
+  def shop_title (name)
+    size = case name.size
+      when 0..16  then 60
+      when 17..20 then 50
+      when 21..26 then 40
+      when 27..34 then 30
+      else 20
+    end
+    "<span style=\"font-size:#{size}px;\">#{name.truncate(50)}</span>".html_safe
+  end
 end
