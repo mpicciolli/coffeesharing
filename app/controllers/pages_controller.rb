fa0aabcdd98e77a69883552491077478aa4e537f
William Hubault <william.hubault@gmail.com>
2013-04-11 02:48:31 +0200

    Add a posters page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,7 @@ class PagesController < ApplicationController
   def home
   end
 
-  def about
-  end
-
-  def contact
+  def posters
   end
 
   def contributors
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Coffeesharing::Application.routes.draw do
 
   root to:'pages#home'
+  resources :places, only:[:index,:show]
+  match 'posters' => 'pages#posters'
   match 'contributors' => 'pages#contributors'
   match 'press' => 'pages#press'
-  resources :places, only:[:index,:show]
 
 end
